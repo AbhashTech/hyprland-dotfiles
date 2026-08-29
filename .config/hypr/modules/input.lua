ab1434b24e5a7a05c77d4a8846a3426e0f7919ee
Kunal Gautam <kunal@ikunal.in>
2026-08-29 15:35:59 +0530

    chore(input): update default keyboard layout configuration

--- .config/hypr/modules/input.lua
+++ .config/hypr/modules/input.lua
@@ -5,8 +5,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Variables/#input
 hl.config({
     input = {
-        kb_layout  = "us,in",
-        kb_variant = ",bolnagri",
+        kb_layout  = "us",
+        kb_variant = "",
         kb_model   = "",
         kb_options = "",
         kb_rules   = "",
